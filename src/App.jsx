7930affcf4dd26570e84557ff52886b57f9af040
Maria Cilloniz <majocilloniz@gmail.com>
2024-11-02 16:41:35 -0700

    Fix on params

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage"
 import UploadPage from "./pages/UploadPage/UploadPage"
 
-
-
 function App() {
 
   return (
@@ -14,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/videos/:id" element={<HomePage />} />
+          <Route path="/videos/:videoId" element={<HomePage />} />
           <Route path="/upload" element={<UploadPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
